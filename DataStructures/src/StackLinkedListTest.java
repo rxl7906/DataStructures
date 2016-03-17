9e48f3a1bf33079afe0d673d0933876b1a9cfb1a
rxl79 <rxl79@10.0.0.9>
2016-03-16 23:50:29 -0400

    Added main test

---
+++ DataStructures/src/StackLinkedListTest.java
@@ -0,0 +1,11 @@
+
+public class StackLinkedListTest {
+	public static void main(String args[]){
+		StackLinkedList sll = new StackLinkedList();
+		sll.push(5);
+		sll.push(2);
+		sll.push(7);
+		sll.push(4);
+		sll.printStack();
+	}
+}
